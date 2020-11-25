c3b75bb6c7cc706b9a447bb887ee62b80d163ce7
Yulia736 <74978497+Yulia736@users.noreply.github.com>
2020-11-25 13:09:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+1.
 print("Введите число A= ")
 A=int(input())
 print("Введите число B= ")
@@ -8,3 +9,16 @@ if A<B:
 if A>B:
 	for i in range (A,B-1,-1):
 		print(i)
+2.
+a=int(input())
+b=int(input())
+c1=a//1000
+c2=a//100%10
+while (((c1*10+c2)*10+c2)*10+c2)<=b:
+    print(c1,c2,c2,c1,sep="")
+    c2=c2+1
+    if (c2>9):
+        c1=c1+1
+        c2=0 
+
+    
